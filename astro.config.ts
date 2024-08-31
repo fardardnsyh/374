ecf6142fec12c950a5ed3322570b2bcda77486e4
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-31 19:41:50 +0700

    Files upload

---
+++ astro.config.ts
@@ -0,0 +1,70 @@
+import { defineConfig, passthroughImageService } from "astro/config";
+import fs from "fs";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import remarkUnwrapImages from "remark-unwrap-images";
+import rehypeExternalLinks from "rehype-external-links";
+import { remarkReadingTime } from "./src/utils/remark-reading-time";
+import vercel from "@astrojs/vercel/static";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://kittichanr-blog.vercel.app/",
+	markdown: {
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		rehypePlugins: [
+			[rehypeExternalLinks, { target: "_blank", rel: ["nofollow, noopener, noreferrer"] }],
+		],
+		remarkRehype: { footnoteLabelProperties: { className: [""] } },
+		shikiConfig: {
+			theme: "dracula",
+			// wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		icon(),
+	],
+	image: {
+		service: passthroughImageService(),
+		// FIXME: update astro version and use below config when release this version https://github.com/withastro/astro/pull/9463
+		// domains: ["astro-cactus.chriswilliams.dev", "localhost"],
+	},
+	// https://docs.astro.build/en/guides/prefetch/
+	prefetch: true,
+	vite: {
+		plugins: [rawFonts([".ttf"])],
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
+	output: "static",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+});
+
+function rawFonts(ext: Array<string>) {
+	return {
+		name: "vite-plugin-raw-fonts",
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore:next-line
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
+}
